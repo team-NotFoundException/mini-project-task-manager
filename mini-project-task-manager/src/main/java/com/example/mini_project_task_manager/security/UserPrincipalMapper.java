fedfe918db77c1f4289acd767d2987de273d8d18
codingisfuk <wwb8524@naver.com>
2025-09-05 17:14:37 +0900

    fix/ture/secret-foder-create

---
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/security/UserPrincipalMapper.java
@@ -0,0 +1,7 @@
+package com.example.mini_project_task_manager.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserPrincipalMapper {
+}
---
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/security/util/PrincipalUtils.java
@@ -0,0 +1,7 @@
+package com.example.mini_project_task_manager.security.util;
+
+
+
+public class PrincipalUtils {
+    private PrincipalUtils() {}
+}
