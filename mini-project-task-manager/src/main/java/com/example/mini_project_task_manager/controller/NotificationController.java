a8dc5b04b78214e36b8e8f60a61b687fefbf6d59
Kitbomin <zzxxcc1433@naver.com>
2025-09-05 15:22:25 +0900

    Controller folder 생성 완료

---
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/controller/NotificationController.java
@@ -0,0 +1,13 @@
+package com.example.mini_project_task_manager.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/notifications")
+@RequiredArgsConstructor
+public class NotificationController {
+    private final  NotificationService notificationService;
+}
