46c53665ecd1b94cdbb79b9eb39d6b9ac7f84078
ehdgn <ehdgn6570@gmail.com>
2025-09-08 17:39:40 +0900

    entity-project-update

--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/entity/Project.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/entity/Project.java
@@ -2,9 +2,13 @@ package com.example.mini_project_task_manager.entity;
 
 import com.example.mini_project_task_manager.entity.base.BaseTimeEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Table(name = "projects")
@@ -22,11 +26,26 @@ public class Project extends BaseTimeEntity {
     private String title;
 
     /** 내용 */
-    @Column(name = "content")
+    @Column(name = "content", length = 255)
     private String content;
 
-//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-//    @JoinColumn(name = "username", nullable = false,
-//            foreignKey = @ForeignKey(name = "fk_projects_user"))
-//    private User user;
+    /** user_id 외래키 설정 */
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(name = "fk_project_user"))
+    private User user;
+
+    @Builder
+    private Project(@NotNull String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
