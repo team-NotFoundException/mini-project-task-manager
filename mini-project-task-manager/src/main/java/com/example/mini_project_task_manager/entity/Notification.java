a9803bf401ee3840ce2e92dd7f1cc28afc1f0e79
Kitbomin <zzxxcc1433@naver.com>
2025-09-08 13:36:44 +0900

    Notification Entity Fix

--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/entity/Notification.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/entity/Notification.java
@@ -2,7 +2,10 @@ package com.example.mini_project_task_manager.entity;
 
 import com.example.mini_project_task_manager.entity.base.BaseTimeEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,6 +20,16 @@ public class Notification extends BaseTimeEntity {
     @Column(name = "id", updatable = false)
     private Long id;
 
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false, foreignKey = @ForeignKey(name = "fk_noti_project"))
+    private Project project;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false, foreignKey = @ForeignKey(name = "fk_noti_author"))
+    private User user;
+
     /** 제목 */
     @Column(name = "title")
     private String title;
@@ -25,9 +38,19 @@ public class Notification extends BaseTimeEntity {
     @Column(name = "content")
     private String content;
 
-//    /** N(공지사항) : 1(USER) 설정 */
-//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-//    @JoinColumn(name = "username", nullable = false,
-//            foreignKey = @ForeignKey(name = "fk_notifications_user"))
-//    private User user;
+
+    @Builder
+    private Notification(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
 }
