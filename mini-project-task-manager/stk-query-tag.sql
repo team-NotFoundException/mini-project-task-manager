fbce0a3b693a15534ad09d2f3f7951418f5b2926
AnyEveryDays <stk9460@naver.com>
2025-09-08 23:41:38 +0900

    STK tasktag table create

--- mini-project-task-manager/stk-query-tag.sql
+++ mini-project-task-manager/stk-query-tag.sql
@@ -5,4 +5,13 @@ CREATE TABLE IF NOT EXISTS `tag`(
     CONSTRAINT `uq_tag_name` UNIQUE (tag_name)
 )
 
+CREATE TABLE IF NOT EXISTS `tasktag` (
+    task_id BIGINT NOT NULL,
+    tag_id BIGINT NOT NULL,
+    CONSTRAINT fk_task FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE,
+    CONSTRAINT fk_tag FOREIGN KEY (tag_id) REFERENCES tag(id) ON DELETE CASCADE
+    CONSTRAINT uq_tasktag UNIQUE (task_id, tag_id)
+);
+
+
 
