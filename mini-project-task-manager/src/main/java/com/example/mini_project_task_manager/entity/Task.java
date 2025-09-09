13ee8cfac03b99b94f58971084cb5e3502981f60
untilthe-end <letsgohangun@gmail.com>
2025-09-09 23:18:21 +0900

    fix/entity-task-update

--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/entity/Task.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/entity/Task.java
@@ -1,6 +1,7 @@
 package com.example.mini_project_task_manager.entity;
 
 import jakarta.validation.constraints.NotNull;
+import jdk.jshell.Snippet;
 import lombok.Builder;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
@@ -35,32 +36,27 @@ public class Task extends BaseTimeEntity {
     @Column(name = "content", nullable = false)
     private String content;
 
-    /** 담당자 */
-    @Column(name = "author", nullable = false, length = 100)
-    private String author;
-
-
     @NotNull
     @ManyToOne
-    @JoinColumn(name = "project_id", nullable = false, foreignKey = @ForeignKey(name = "fk_task_project"))
+    @JoinColumn(name = "project_id", nullable = false, foreignKey = @ForeignKey(name = "fk_tasks_project"))
     private Project project;
 
     @NotNull
     @ManyToOne
-    @JoinColumn(name = "author_id", nullable = false, foreignKey = @ForeignKey(name = "fk_task_user"))
+    @JoinColumn(name = "author_id", nullable = false, foreignKey = @ForeignKey(name = "fk_tasks_user"))
     private User user;
 
     // Task N <-> Tag N 다대다 관계
-    @ElementCollection(fetch = FetchType.LAZY)
+    // @ElementCollection(fetch = FetchType.LAZY)
     // 할일 리스트목록에 tag가 바로 안보이게 설정할 때, 이거 주석처리하면 Set<Tag>에 오류안남.
-    @ManyToMany
-    @CollectionTable(   // `task_tags` table 만들때 아래와 비교할것
-            name = "task_tags",
-            joinColumns = @JoinColumn(name = "task_id", foreignKey = @ForeignKey(name = "fk_task_tags"))
-            , uniqueConstraints = @UniqueConstraint(name = "uk_task_tags", columnNames = {"task_id", "tag"})
-    )
-    @Column(name = "tag", length = 30)
-    @JoinColumn(name = "tag_id", foreignKey = @ForeignKey(name = "fk_task_tag"))
+    @ManyToMany(mappedBy = "task", cascade = CascadeType.ALL, orphanRemoval = true)
+//    @CollectionTable(   // `task_tags` table 만들때 아래와 비교할것
+//            name = "task_tags",
+//            joinColumns = @JoinColumn(name = "task_id", foreignKey = @ForeignKey(name = "fk_task_tags"))
+//            , uniqueConstraints = @UniqueConstraint(name = "uq_task_tags", columnNames = {"task_id", "tag"})
+//    )
+//    @Column(name = "tag", length = 30)
+//    @JoinColumn(name = "tag_id", foreignKey = @ForeignKey(name = "fk_task_tag"))
     @Enumerated(EnumType.STRING)
     private Set<Tag> tags = new HashSet<>(); // 중복 제거위해 Set 사용
 
@@ -71,30 +67,26 @@ public class Task extends BaseTimeEntity {
     /** 상태 */
     @Enumerated(EnumType.STRING)
     @Column(name = "status", nullable = false, length = 50)
-    private Status status;
+    private Status status = Status.TODO;        // 디폴트 값 설정
 
     /** 우선 순위 */
     @Enumerated(EnumType.STRING)
     @Column(name = "priority", nullable = false, length = 50)
-    private Priority priority;
+    private Priority priority = Priority.MEDIUM;
 
-    /** 매개변수 5개지만 필수 3개만 선택해서 build 가능 */
     @Builder
     public Task(@NotNull String title, @NotNull String content,
-                @NotNull String author, Status status, Priority priority) {
+                @NotNull User user, Status status, Priority priority) {
         this.title = title;
         this.content = content;
-        this.author = author;
-        this.status = (status == null ? Status.TODO : status);
-        this.priority = (priority == null ? Priority.MEDIUM : priority);
-        this.tags = (tags == null || tags.isEmpty())? new HashSet<>(Set.of(Tag)) : tags;
+        this.user = user;
+        this.status = (status != null) ? status : Status.TODO;
+        this.priority = (priority != null) ? priority : Priority.MEDIUM;
+        this.tags = (tags == null || tags.isEmpty())? new HashSet<>(Set.of()) : tags;
     }
 
-    void setTag(Tag tag) {
-        this.tag = tag;
+    void addTags(Tag tag) {
+        tags.add(tag); // Task 클래스의 tags필드 변수에 tag빌더로 만들어진 tag들을 추가한다.
+        tag.setTask(this); // Tag 클래스에 있어야 할까.?
     }
 }
-
-/*
-    65번줄 / 90번줄 어떻게 해야할지 모르겠음.
- */
