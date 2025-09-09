515883b2fbced54986f05c9e2579dc28fbd7d7da
Kitbomin <zzxxcc1433@naver.com>
2025-09-09 15:34:47 +0900

    comment and notification repository error fix

--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/repository/CommentsRepository.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/repository/CommentsRepository.java
@@ -1,8 +1,9 @@
 package com.example.mini_project_task_manager.repository;
 
+import com.example.mini_project_task_manager.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentsRepository extends JpaRepository<> {
+public interface CommentsRepository extends JpaRepository<Comment, Long> {
 }
--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/repository/NotificationRepository.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/repository/NotificationRepository.java
@@ -1,8 +1,9 @@
 package com.example.mini_project_task_manager.repository;
 
+import com.example.mini_project_task_manager.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NotificationRepository extends JpaRepository<> {
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
 }
