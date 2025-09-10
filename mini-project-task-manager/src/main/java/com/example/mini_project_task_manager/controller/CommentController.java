890f61bbee9125c1cc6385034152e46efe55b51b
Kitbomin <zzxxcc1433@naver.com>
2025-09-10 19:18:08 +0900

    Comment Controller 생성,수정,삭제 기ì 정의 및 Service에 구현

--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/controller/CommentController.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/controller/CommentController.java
@@ -1,14 +1,53 @@
 package com.example.mini_project_task_manager.controller;
 
 
+import com.example.mini_project_task_manager.DTO.ResponseDto;
+import com.example.mini_project_task_manager.DTO.commentDto.request.CommentRequest;
+import com.example.mini_project_task_manager.DTO.commentDto.response.CommentResponse;
+import com.example.mini_project_task_manager.common.constants.ApiMappingPattern;
 import com.example.mini_project_task_manager.service.CommentService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/comments")
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
+
+
+    // 댓글 생성
+    @PostMapping
+    public ResponseEntity<ResponseDto<CommentResponse>> createComment(
+            @PathVariable("taskId") @Positive(message = "taskId는 1 이상이어야합니당") Long taskId,
+            @Valid @RequestBody CommentRequest.CommentCreateRequest dto
+            ) {
+        ResponseDto<CommentResponse> response = commentService.createComment(taskId, dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    // 댓글 수정
+    @PutMapping("/api/v1/tasks/{taskId}/comments/{commentsId}")
+    public ResponseEntity<ResponseDto<CommentResponse>> updateComment(
+            @PathVariable("taskId") @Positive(message = "taskId는 1 이상이어야합니당") Long taskId,
+            @PathVariable("commentId") @Positive(message = "commentId는 1 이상이어야합니당") Long commentId,
+            @Valid @RequestBody CommentRequest.CommentUpdateRequest dto
+    ) {
+        ResponseDto<CommentResponse> response = commentService.updateComment(taskId, commentId, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    // 댓글 삭제
+    @DeleteMapping("/api/v1/tasks/{taskId}/comments/{commentsId}")
+    public ResponseEntity<ResponseDto<CommentResponse>> deleteComment(
+            @PathVariable("taskId") @Positive(message = "taskId는 1 이상이어야합니당") Long taskId,
+            @PathVariable("commentId") @Positive(message = "commentId는 1 이상이어야합니당") Long commentId
+    ){
+        ResponseDto<CommentResponse> response = commentService.deleteComment(taskId, commentId);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 }
