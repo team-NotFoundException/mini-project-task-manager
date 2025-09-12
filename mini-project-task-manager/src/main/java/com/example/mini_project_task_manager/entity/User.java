c45bd82d385ba2b926fe11bc784098c2281c5cdb
kim-se-hooh <wwb8524@naver.com>
2025-09-12 15:23:10 +0900

    test

--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/entity/User.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/entity/User.java
@@ -54,7 +54,6 @@ public class User extends BaseTimeEntity {
     @Column(name = "gender", length = 20)
     private Gender gender;
 
-    private Set<RoleType> roles = new HashSet<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<UserRole> userRoles = new HashSet<>();
@@ -68,5 +67,6 @@ public class User extends BaseTimeEntity {
         this.nickname = nickname;
         this.gender = gender;
     }
+
     
 }
