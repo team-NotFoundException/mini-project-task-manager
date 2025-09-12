5f5bd426ec27e2ab18db72d59832635762df427e
kim-se-hooh <wwb8524@naver.com>
2025-09-12 11:39:22 +0900

    임시저장

--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/filter/JwtAuthenticationFilter.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/filter/JwtAuthenticationFilter.java
@@ -1,11 +1,32 @@
 package com.example.mini_project_task_manager.filter;
 
+import com.example.mini_project_task_manager.provider.JwtProvider;
+import com.example.mini_project_task_manager.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+
 @Component
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends OncePerRequestFilter{
-  
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+    private final UserRepository userRepository;
+
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException
+    {
+
+    }
 }
