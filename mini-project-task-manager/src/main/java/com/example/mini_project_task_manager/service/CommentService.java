1358a280d0c3d4fa4aa23bda562cff4c70518b7a
Kitbomin <zzxxcc1433@naver.com>
2025-09-12 15:35:19 +0900

    오류 최종 수정 완료

--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/service/CommentService.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/service/CommentService.java
@@ -1,15 +1,15 @@
-//package com.example.mini_project_task_manager.service;
-//
-//import com.example.mini_project_task_manager.dto.ResponseDto;
-//import com.example.mini_project_task_manager.dto.comment.request.CommentRequest;
-//import com.example.mini_project_task_manager.dto.comment.response.CommentResponse;
-//import jakarta.validation.Valid;
-//import jakarta.validation.constraints.Positive;
-//
-//public interface CommentService {
-//    ResponseDto<CommentResponse> createComment(@Positive(message = "taskId는 1 이상이어야합니당") Long taskId, CommentRequest.@Valid CommentCreateRequest dto);
-//
-//    ResponseDto<CommentResponse> updateComment(@Positive(message = "taskId는 1 이상이어야합니당") Long taskId, @Positive(message = "commentId는 1 이상이어야합니당") Long commentId, CommentRequest.@Valid CommentUpdateRequest dto);
-//
-//    ResponseDto<CommentResponse> deleteComment(@Positive(message = "taskId는 1 이상이어야합니당") Long taskId, @Positive(message = "commentId는 1 이상이어야합니당") Long commentId);
-//}
+package com.example.mini_project_task_manager.service;
+
+import com.example.mini_project_task_manager.dto.ResponseDto;
+import com.example.mini_project_task_manager.dto.comment.request.CommentRequest;
+import com.example.mini_project_task_manager.dto.comment.response.CommentResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+
+public interface CommentService {
+    ResponseDto<CommentResponse> createComment(@Positive(message = "taskId는 1 이상이어야합니당") Long taskId, CommentRequest.@Valid CommentCreateRequest dto);
+
+    ResponseDto<CommentResponse> updateComment(@Positive(message = "taskId는 1 이상이어야합니당") Long taskId, @Positive(message = "commentId는 1 이상이어야합니당") Long commentId, CommentRequest.@Valid CommentUpdateRequest dto);
+
+    ResponseDto<CommentResponse> deleteComment(@Positive(message = "taskId는 1 이상이어야합니당") Long taskId, @Positive(message = "commentId는 1 이상이어야합니당") Long commentId);
+}
