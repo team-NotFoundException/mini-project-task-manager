323129f8e056325ac367b30cc7063ec5085a17cb
kim-se-hooh <wwb8524@naver.com>
2025-09-15 14:57:42 +0900

    Sign로그ìã ë ë¡회ã원관련 Request

--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/dto/Auth/request/PasswordChangeRequest.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/dto/Auth/request/PasswordChangeRequest.java
@@ -3,7 +3,10 @@ package com.example.mini_project_task_manager.dto.Auth.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record PasswordChangeRequest(
-        @NotBlank @Size(min = 8, max = 100)
-        String newPassword
-) {}
+public class PasswordChangeRequest {
+        public record PasswordRequest (
+                @NotBlank(message =  "비밀번호 입력은 필수 입니다.")
+                @Size(min = 8, max = 16, message = "비밀번호는 8자 이상 입력 해야 합니다. ")
+                String newPassword
+        ) {}
+}
--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/repository/AuthRepository.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/repository/AuthRepository.java
@@ -1,8 +1,10 @@
-//package com.example.mini_project_task_manager.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface AuthRepository extends JpaRepository<> {
-//}
+package com.example.mini_project_task_manager.repository;
+
+import com.example.mini_project_task_manager.common.enums.Auth;
+import com.example.mini_project_task_manager.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthRepository extends JpaRepository<Auth, Long> {
+}
