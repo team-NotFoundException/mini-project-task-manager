5dec3aba19fcadff06da0d81fb05eeb73543a09a
Kitbomin <zzxxcc1433@naver.com>
2025-09-17 18:50:38 +0900

    fix Comment rename

--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/dto/comment/request/CommentRequest.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/dto/comment/request/CommentRequest.java
@@ -8,13 +8,13 @@ public class CommentRequest {
     public record CommentCreateRequest (
             @NotBlank(message = "댓글 입력은 필 수 입니다.")
             @Size(max = 300, message = "댓글은 300자를 넘길 수 없습니다.")
-            String content
+            String comment
     ) {}
 
     /** 댓글 수정 요청 DTO */
     public record CommentUpdateRequest(
             @NotBlank(message = "댓글 내용은 공백일 수 없습니다.")
             @Size(max = 300, message = "댓글은 300자를 넘길 수 없습니다.")
-            String content
+            String comment
     ) {}
 }
