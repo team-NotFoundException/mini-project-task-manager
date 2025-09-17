5dfc9fcd7f925aa9b58d49491fa92d5552acd53c
Kitbomin <zzxxcc1433@naver.com>
2025-09-17 15:13:06 +0900

    Comment Repository fix

--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/repository/CommentsRepository.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/repository/CommentsRepository.java
@@ -16,21 +16,25 @@ public interface CommentsRepository extends JpaRepository<Comment, Long> {
         String Content();
         String getAuthor();
     }
+
+    // keyword가 포함된 댓글 찾기
     @Query(value = """
         select 
-            id,
+            id          as commentId,
             content
-        from comment
+        from comments
             where content like concat('%', :searchKeyword, '%') 
             order by content desc
     """, nativeQuery = true)
     List<Comment> findByCommentKeyword(@Param("searchKeyword") String searchKeyword);
 
+    // 입력한 작성자와 일치하는 작성자의 모든 댓글 가져오기
     @Query(value = """
         select 
-            id,
-            author
-        from comment
+            content     as content,
+            author_name as authorName
+        from comments c left join users u
+            on c.authorId = u.userId 
             where auth like concat('%', :author, '%')
             order by author desc
 """,nativeQuery = true)
