a5576306862e1f780c065d2ed9984538e21c952a
ehdgn <ehdgn6570@gmail.com>
2025-09-22 14:28:42 +0900

    project-serviceImpl update

--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/service/impl/ProjectServiceImpl.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/service/impl/ProjectServiceImpl.java
@@ -9,10 +9,12 @@ import com.example.mini_project_task_manager.repository.ProjectRepository;
 import com.example.mini_project_task_manager.repository.UserRepository;
 import com.example.mini_project_task_manager.security.UserPrincipal;
 import com.example.mini_project_task_manager.service.ProjectService;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -67,7 +69,7 @@ public class ProjectServiceImpl implements ProjectService {
                 .map(ProjectResponse.ProjectSummaryResponse::from)
                 .toList();
 
-        if (result.isEmpty()) throw new IllegalArgumentException("프로젝트가 없습니다.");
+        if (result.isEmpty()) throw new EntityNotFoundException("프로젝트가 없습니다.");
 
         return ResponseDto.setSuccess("조회 완료", result);
     }
@@ -135,6 +137,8 @@ public class ProjectServiceImpl implements ProjectService {
     }
 
     private void validateTitle(String title) {
-        throw new IllegalArgumentException("제목을 입력해주세요");
+        if (!StringUtils.hasText(title)) {
+            throw new IllegalArgumentException("제목을 입력해주세요");
+        }
     }
 }
