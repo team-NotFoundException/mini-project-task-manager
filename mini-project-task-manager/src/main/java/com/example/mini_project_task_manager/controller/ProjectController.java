ced3861de160576edd49c85fc2360ea623b00fbb
ehdgn <ehdgn6570@gmail.com>
2025-09-25 11:53:57 +0900

    project-dto,repository update

--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/controller/ProjectController.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/controller/ProjectController.java
@@ -2,6 +2,7 @@ package com.example.mini_project_task_manager.controller;
 
 
 import com.example.mini_project_task_manager.common.constants.ApiMappingPattern;
+import com.example.mini_project_task_manager.common.enums.Sorted;
 import com.example.mini_project_task_manager.dto.ResponseDto;
 import com.example.mini_project_task_manager.dto.project.request.ProjectRequest;
 import com.example.mini_project_task_manager.dto.project.response.ProjectResponse;
@@ -24,8 +25,6 @@ import java.util.List;
 public class ProjectController {
     private final ProjectService projectService;
 
-    // gg
-
     // 1) 프로젝트 생성
     @PreAuthorize("hasAnyRole('MANAGER', 'ADMIN')")
     @PostMapping
@@ -41,7 +40,7 @@ public class ProjectController {
     @PreAuthorize("hasAnyRole('USER', 'MANAGER', 'ADMIN')")
     @GetMapping(ApiMappingPattern.Projects.SORTED)
     public ResponseEntity<ResponseDto<List<ProjectResponse.ProjectSummaryResponse>>> getAllProjectsOrderByCreatedAt(
-            @RequestParam("sortedBy") boolean sortedBy
+            @RequestParam("sortedBy") String sortedBy
     ) {
         ResponseDto<List<ProjectResponse.ProjectSummaryResponse>> response = projectService.getAllProjectsOrderByCreatedAt(sortedBy);
         return ResponseEntity.status(HttpStatus.OK).body(response);
--- mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/dto/project/response/ProjectResponse.java
+++ mini-project-task-manager/src/main/java/com/example/mini_project_task_manager/dto/project/response/ProjectResponse.java
@@ -1,10 +1,10 @@
 package com.example.mini_project_task_manager.dto.project.response;
 
+import com.example.mini_project_task_manager.common.utils.DateUtils;
 import com.example.mini_project_task_manager.entity.Project;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import java.time.LocalDateTime;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProjectResponse {
@@ -15,7 +15,10 @@ public class ProjectResponse {
             Long id,
             String title,
             String author,
-            LocalDateTime createdAt
+            String createdAtKst,
+            String createdUtcIso,
+            String updatedAtKst,
+            String updatedUtcIso
     ) {
         public static ProjectSummaryResponse from(Project project) {
             if (project == null) return null;
@@ -24,7 +27,10 @@ public class ProjectResponse {
                     project.getId(),
                     project.getTitle(),
                     project.getUser().getNickname(),
-                    project.getCreatedAt()
+                    DateUtils.toKstString(project.getCreatedAt()),
+                    DateUtils.toUtcString(project.getCreatedAt()),
+                    DateUtils.toKstString(project.getUpdatedAt()),
+                    DateUtils.toUtcString(project.getUpdatedAt())
             );
         }
     }
@@ -35,8 +41,8 @@ public class ProjectResponse {
             Long id,
             String title,
             String content,
-            String author,
-            LocalDateTime createdAt
+            String author
+
     ) {
         public static ProjectDetailResponse from(Project project) {
             if (project == null) return null;
@@ -45,8 +51,7 @@ public class ProjectResponse {
                     project.getId(),
                     project.getTitle(),
                     project.getContent(),
-                    project.getUser().getNickname(),
-                    project.getCreatedAt()
+                    project.getUser().getNickname()
             );
         }
     }
